93d6147003ade6f70d495d484b2f016d97d167a7
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-02 21:23:23 +0300

    Add the test cases of the respective classes.

---
+++ images/tests.py
@@ -0,0 +1,33 @@
+from django.test import TestCase
+from .models import Image,Location,Category
+
+# Create your tests here.
+class LocationTestClass(TestCase):
+    # Set Up method
+    def setUp(self):
+        self.germany = Location(name = 'Germany')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.germany,Location))
+
+    # Testing save method
+    def test_save_method(self):
+        self.germany.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)
+
+class CategoryTestClass(TestCase):
+    # Set Up Method
+    def setUp(self):
+        self.saloon = Category(name = 'Saloon')
+
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.saloon,Category))
+
+    # Testing save method
+    def test_saving_method(self):
+        self.saloon.save_category()
+        categories = Category.objects.all()        
+        self.assertTrue(len(categories) > 0)
