b3e16562c60853798993db6f03420d7a85cb442d
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-03 15:11:36 +0300

    Capitalize the name 'home'

--- images/urls.py
+++ images/urls.py
@@ -4,7 +4,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-    url('^$',views.home,name = 'home'),
+    url('^$',views.home,name = 'Home'),
     url(r'^search/', views.search_results, name='search_results'),
     url(r'^image/(\d+)', views.image, name = 'image')
    
