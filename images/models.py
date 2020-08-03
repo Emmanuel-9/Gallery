7746217fbf636b5a04272a5a4c11daef915507ca
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-03 15:10:58 +0300

    Replace title with name.

--- images/models.py
+++ images/models.py
@@ -31,5 +31,5 @@ class Image(models.Model):
     
     @classmethod
     def search_by_name(cls,search_term):
-        images = cls.objects.filter(title__icontains = search_term)
+        images = cls.objects.filter(name__icontains = search_term)
         return images
