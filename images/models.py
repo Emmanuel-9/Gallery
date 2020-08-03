45f0cfa1fcd699ce5722ca4d91bfe72ad23cf8fb
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-03 06:33:27 +0300

    Add the search_by_name function.

--- images/models.py
+++ images/models.py
@@ -23,8 +23,13 @@ class Image(models.Model):
     actual_image = models.ImageField(upload_to = 'pictures/')
     name = models.CharField(max_length = 30)
     description = models.CharField(max_length = 60)
-    location = models.ForeignKey(Location)
-    category = models.ForeignKey(Category)
+    location = models.ForeignKey(Location,on_delete = models.CASCADE)
+    category = models.ForeignKey(Category,on_delete = models.CASCADE)
 
     def __str__(self):
         return self.name
+    
+    @classmethod
+    def search_by_name(cls,search_term):
+        images = cls.objects.filter(title__icontains = search_term)
+        return images
