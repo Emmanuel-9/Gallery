11429e3ae7da145f49c9528410982f3846e035b5
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-04 07:03:28 +0300

    Add the del_location and get_location functions.

--- images/models.py
+++ images/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 
+
 # Create your models here.
 class Location(models.Model):
     name = models.CharField(max_length = 30)
@@ -8,7 +9,16 @@ class Location(models.Model):
         return self.name 
 
     def save_location(self):
-        self.save()    
+        self.save()  
+
+    def del_location(self):
+        self.delete() 
+
+    @classmethod
+    def get_location(cls):
+        locations = Location.objects.all()
+        return locations
+
     
 class Category(models.Model):
     name = models.CharField(max_length = 30)
@@ -17,7 +27,18 @@ class Category(models.Model):
         return self.name
 
     def save_category(self):
-        self.save()    
+        self.save()   
+
+    def del_category(self):
+        self.delete()     
+
+    @classmethod
+    def get_category(cls):
+        categories = Category.objects.all()
+        return categories
+
+
+      
 
 class Image(models.Model):
     actual_image = models.ImageField(upload_to = 'pictures/')
@@ -28,8 +49,21 @@ class Image(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save_image(self):
+        self.save()
+
+    def del_image(self):
+        self.delete()
+
+               
     
     @classmethod
     def search_by_name(cls,search_term):
         images = cls.objects.filter(name__icontains = search_term)
         return images
+
+    @classmethod
+    def search_by_location(cls,location):
+        image_location = cls.objects.filter(location__name = location)
+        return image_location
