f8fa9803c39561af8a133ae5dddf04dfabd5776a
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-04 07:05:41 +0300

    Add the url pattern for images.

--- images/urls.py
+++ images/urls.py
@@ -6,8 +6,11 @@ from . import views
 urlpatterns=[
     url('^$',views.home,name = 'Home'),
     url(r'^search/', views.search_results, name='search_results'),
-    url(r'^image/(\d+)', views.image, name = 'image')
+    url(r'^image/(\d+)', views.image, name = 'image'),
+    url(r'^location/',views.location, name = 'Location'),
+    url(r'^category/',views.category, name = 'category') 
+    
    
 ]
-if settings.DEBUG:
+if settings.DEBUG: 
     urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
