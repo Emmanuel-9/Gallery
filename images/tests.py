3cde010e16f8848318cedba87c25389e14d15aeb
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-04 07:04:50 +0300

    Add the test_del_method for LOcationTestClass.

--- images/tests.py
+++ images/tests.py
@@ -14,8 +14,15 @@ class LocationTestClass(TestCase):
     # Testing save method
     def test_save_method(self):
         self.germany.save_location()
-        locations = Location.object.all()
+        locations = Location.objects.all()
         self.assertTrue(len(locations) > 0)
+
+    def test_del_method(self):
+        self.germany.save_location()
+        test_location = Location(name = 'Paris')
+        test_location.save_location()
+        locations = Location.objects.all()
+        self.assertTrue(len(locations) > 0)    
 
 class CategoryTestClass(TestCase):
     # Set Up Method
@@ -29,5 +36,25 @@ class CategoryTestClass(TestCase):
     # Testing save method
     def test_saving_method(self):
         self.saloon.save_category()
-        categories = Category.object.all()        
+        categories = Category.objects.all()        
         self.assertTrue(len(categories) > 0)
+
+    def test_delete_method(self):
+        self.saloon.save_category()  
+        test_category = Category(name = 'SUV')
+        test_category.save_category()
+        categories = Category.objects.all()
+        self.saloon.del_category()
+        self.assertTrue(len(categories) > 0)  
+
+class ImageTestCase(TestCase):
+    # Set Up Method
+    def setUp(self):
+        self.image = Image(actual_image = 'image.jpg', name = 'Fortuner', description = 'elegant')
+    
+    # Testing instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.image,Image))
+
+     
+        
