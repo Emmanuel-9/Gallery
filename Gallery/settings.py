5e65a1c2319bd212745b6c131f74c25ecf74a74b
Emmanuel-9 <oregadaniel181@gmail.com>
2020-08-12 15:12:46 +0300

    Remove the overwritten environment variables.

--- Gallery/settings.py
+++ Gallery/settings.py
@@ -53,12 +53,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.11/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '66tdhjfi7y38udyy'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
 
 
 # Application definition
